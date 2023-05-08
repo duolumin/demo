9fa3a21f177d3286cac9842585b5b8f5e50828ba
duolumin <80921970@qq.com>
2023-05-08 15:48:55 +0800

    第2次提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("第1次提交");
+        System.out.println("第2次提交");
         SpringApplication.run(DemoApplication.class, args);
+
     }
 
 }
