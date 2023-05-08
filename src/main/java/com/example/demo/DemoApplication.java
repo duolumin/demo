af9b4f1ebcd88157283883120911c6ba76609486
duolumin <80921970@qq.com>
2023-05-08 15:50:35 +0800

    第3次提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("第1次提交");
         System.out.println("第2次提交");
+        System.out.println("第3次提交");
         SpringApplication.run(DemoApplication.class, args);
 
     }
